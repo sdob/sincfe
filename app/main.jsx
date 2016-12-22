4ad805a3cc353a11ff919e38a15586e41cda3908
sdob <sdob@cs.ucc.ie>
2016-12-22 17:42:12 +0000

    Comment main.jsx

--- app/main.jsx
+++ app/main.jsx
@@ -6,9 +6,9 @@ import { Provider } from 'react-redux';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Router, browserHistory } from 'react-router';
 import reduxThunk from 'redux-thunk';
+import { reducer as formReducer } from 'redux-form';
 
 // Import reducers from submodules
-import { reducer as formReducer } from 'redux-form';
 import authReducer from './auth/reducers';
 import coursesReducer from './courses/reducers';
 import clubQualificationsReducer from './club-qualifications/reducers';
@@ -17,15 +17,18 @@ import currentMembershipStatusReducer from './membership-history/reducers';
 import qualificationsReducer from './member-qualifications/reducers';
 import regionsReducer from './regions/reducers';
 
-// Import action types
+// import authentication action types (in case we're authenticated at
+// startup)
 import * as authActionTypes from './auth/types';
-// Import our main app
+
 // Import routes
 import routes from './routes';
-// Require styles
+
+// Require styles to be bundled; linting is disabled on this line because
+// eslint thinks that we're defining something that's never used
 import styles from './styles/main.scss'; // eslint-disable-line
 
-// Combine reducers
+// Combine our submodule reducers
 const rootReducer = combineReducers({
   auth: authReducer,
   clubQualifications: clubQualificationsReducer,
@@ -41,15 +44,17 @@ const rootReducer = combineReducers({
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
 const store = createStoreWithMiddleware(rootReducer);
 
-// Check whether we're logged in at page load
+// Check whether we're logged in at page load and dispatch an action
 const token = cookie.load('token');
 if (token) {
-  // TODO: Check that this token is OK (in case it's expired) and redirect
-  // to login
+  // TODO: Check that this token is OK (in case it's expired) and redirect to login
+  // Configure Axios to send the token in the header for all requests
   axios.defaults.headers.common.Authorization = `Token ${token}`;
+  // Dispatch a LOGIN_SUCCESS
   store.dispatch({ type: authActionTypes.LOGIN_SUCCESS });
 }
 
+// Render the app
 ReactDOM.render(
   <Provider store={store}>
     <Router history={browserHistory} routes={routes} />
