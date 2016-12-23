b6859d6a83739d0bfc55a01cfc832643f54693ae
sdob <sdob@cs.ucc.ie>
2016-12-23 14:42:26 +0000

    Prefix path constants with slash

--- app/paths.js
+++ app/paths.js
@@ -11,20 +11,20 @@
  * (3) the string values are lower-case, with words connected by hyphens.
  */
 
-export const ADD_MEMBER = 'add-member';
-export const CLUB_DETAILS = 'club-details';
-export const CLUB_QUALIFICATIONS = 'club-qualifications';
-export const CLUB_RENEWAL_ORDERS = 'club-renewal-orders';
-export const CONTACT_US = 'contact-us';
-export const DOCUMENTS = 'documents';
-export const HELP_PAGE = 'help';
-export const LOGIN = 'login';
-export const LOGOUT = 'logout';
-export const MEDICAL_DISCLAIMER = 'medical-disclaimer';
-export const MEMBER_QUALIFICATIONS = 'member-qualifications';
-export const MEMBERSHIP_HISTORY = 'membership-history';
-export const ORGANIZE_COURSES = 'organize-courses';
-export const PROFILE = 'edit-profile';
-export const SEARCH_CURRENT_MEMBERS = 'search-current-members';
-export const TEACH_COURSES = 'teach-courses';
-export const VIEW_COURSES = 'view-courses';
+export const ADD_MEMBER = '/add-member';
+export const CLUB_DETAILS = '/club-details';
+export const CLUB_QUALIFICATIONS = '/club-qualifications';
+export const CLUB_RENEWAL_ORDERS = '/club-renewal-orders';
+export const CONTACT_US = '/contact-us';
+export const DOCUMENTS = '/documents';
+export const HELP_PAGE = '/help';
+export const LOGIN = '/login';
+export const LOGOUT = '/logout';
+export const MEDICAL_DISCLAIMER = '/medical-disclaimer';
+export const MEMBER_QUALIFICATIONS = '/member-qualifications';
+export const MEMBERSHIP_HISTORY = '/membership-history';
+export const ORGANIZE_COURSES = '/organize-courses';
+export const PROFILE = '/edit-profile';
+export const SEARCH_CURRENT_MEMBERS = '/search-current-members';
+export const TEACH_COURSES = '/teach-courses';
+export const VIEW_COURSES = '/view-courses';
