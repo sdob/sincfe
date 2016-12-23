fa97400127de96d832209e77ffc8a3f513daf5d2
sdob <sdob@cs.ucc.ie>
2016-12-23 12:02:55 +0000

    Document sidebar components

--- app/sidebar/Sidebar.jsx
+++ app/sidebar/Sidebar.jsx
@@ -7,12 +7,27 @@ import MemberOptions from './MemberOptions';
 import TreasurerOptions from './TreasurerOptions';
 
 class Sidebar extends Component {
+  // Render the sidebar menu. This is made up of several submenus, which
+  // contain links to actions that users with certain privileges can
+  // take. So Dive Officers see the Dive Officer options, Treasurers
+  // see the Treasurer options, and so on. The API server returns
+  // a list of committee positions as part of the profile, so we'll
+  // use that.
+  //
+  // A quick reminder here that even if a user manually edits the profile
+  // object in local storage to give themselves these roles, nothing is
+  // leaked, since privileges to access the data that populate the
+  // destination pages are checked on the server independently. We're
+  // just selectively showing/hiding these menus as a convenience to
+  // the user.
   render() {
-    if (!this.props.profile) {
+    const { profile } = this.props;
+
+    // If the profile hasn't loaded yet, don't show anything.
+    if (!profile) {
       return null;
     }
 
-    const { profile } = this.props;
     return (
       <div>
         {profile.readable_committee_positions.includes('Dive Officer') && <DivingOfficerOptions />}
--- app/sidebar/SidebarItem.jsx
+++ app/sidebar/SidebarItem.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router';
 
+/*
+ * A simple presentational component that takes 'icon' and 'text'
+ * properties (and, optionally, extra classNames) and returns the
+ * HTML we want. It keeps things consistent and saves us boilerplate
+ * in the sidebar components.
+ */
 export default function SidebarItem(props) {
   return (
     <li className={`sinc-sidebar__menu-item ${props.className}`}>
