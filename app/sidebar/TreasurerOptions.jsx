80cb49e2d51f22d02bea8654ba544aecc234fe88
sdob <sdob@cs.ucc.ie>
2016-12-28 19:07:31 +0000

    Add correct route to search-current-members

--- app/sidebar/TreasurerOptions.jsx
+++ app/sidebar/TreasurerOptions.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import SidebarItem from './SidebarItem';
 
+import * as paths from '../paths';
+
 export default function TreasurerOptions() {
   return (
     <div className="sinc-sidebar__section">
@@ -8,7 +10,7 @@ export default function TreasurerOptions() {
         <h2>Treasurer options</h2>
       </div>
       <ul className="nav sinc-sidebar__menu">
-        <SidebarItem icon="search" to="SearchMembers" text="Search current members" />
+        <SidebarItem icon="search" to={paths.SEARCH_CURRENT_MEMBERS} text="Search current members" />
         <SidebarItem icon="money" to="RenewalOrders" text="Renewal orders" />
       </ul>
     </div>
