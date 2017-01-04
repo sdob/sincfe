5b7565576577a79c888d49e802ab940cfdf74780
sdob <sdob@cs.ucc.ie>
2017-01-04 13:52:40 +0000

    Add search-members item to DO Options

--- app/sidebar/DivingOfficerOptions.jsx
+++ app/sidebar/DivingOfficerOptions.jsx
@@ -10,6 +10,7 @@ export default function DivingOfficerOptions() {
       menuTitle="Diving Officer options"
       menuId="js-divingOfficerOptions"
     >
+      <SidebarItem icon="search" to={paths.SEARCH_CURRENT_MEMBERS} text="Search current members" />
       <SidebarItem icon="plus" to={paths.ADD_MEMBER} text="Add member" />
       <SidebarItem icon="money" to={paths.CLUB_RENEWAL_ORDERS} text="Renewal orders" />
       <SidebarItem icon="certificate" to={paths.CLUB_QUALIFICATIONS} text="Qualifications" />
