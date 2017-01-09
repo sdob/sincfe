4ab28a5776065bc6d4df465e271426e3b9430541
sdob <sdob@cs.ucc.ie>
2017-01-09 18:56:50 +0000

    Use MemberTable in SearchCurrentMembers

--- app/search-current-members/SearchCurrentMembers.jsx
+++ app/search-current-members/SearchCurrentMembers.jsx
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { compose } from 'redux';
+import * as Table from 'reactabular-table';
+import * as resolve from 'table-resolver';
+import orderBy from 'lodash/orderBy';
+import * as sort from 'sortabular';
+
+import MemberTable from '../shared/MemberTable';
 import PageError from '../shared/PageError';
 import PageLoading from '../shared/PageLoading';
 import fetchCurrentMembers from './actions';
 
-class SearchCurrentMembers extends Component {
+class SearchCurrentMembers extends MemberTable {
 
   componentDidMount() {
     const { profile } = this.props;
@@ -29,15 +36,44 @@ class SearchCurrentMembers extends Component {
       return (<PageLoading />);
     }
 
-    const members = currentMembers.currentMembers;
-
     // TODO: Set 'scope' to whatever scope the user is viewing
     // (e.g., club, region, etc.)
     const scope = profile.club.name;
 
+    const members = currentMembers.currentMembers;
+
+    const rows = members;
+
+
+    const { columns, sortingColumns } = this.state;
+    const resolvedColumns = resolve.columnChildren({ columns });
+
+    const sortedRows = compose(
+      sort.sorter({
+        columns: resolvedColumns,
+        sortingColumns,
+        sort: orderBy,
+        strategy: sort.strategies.byProperty,
+      }),
+      resolve.resolve({
+        columns: resolvedColumns,
+        method: resolve.nested,
+      })
+    )(rows);
+
     return (
       <div>
         <h1 className="sinc-page-header">Search current members ({scope})</h1>
+        <Table.Provider
+          className="table pure-table pure-table-striped"
+          columns={columns}
+        >
+          <Table.Header
+            headerRows={resolve.headerRows({ columns })}
+          />
+          <Table.Body rows={sortedRows} rowKey="id" />
+        </Table.Provider>
+        {/*
         <table className="table">
           <thead>
             <tr>
@@ -66,6 +102,7 @@ class SearchCurrentMembers extends Component {
               ))}
           </tbody>
         </table>
+        */}
       </div>
     );
   }
