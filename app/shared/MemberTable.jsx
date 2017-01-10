824dba1963fc1046fd8f43d34d02f3eb9b823e7c
sdob <sdob@cs.ucc.ie>
2017-01-10 19:20:30 +0000

    Sort users in MemberTable by ID, descending
    This commit reverses the default order in which users are sorted by
    default in MemberTable. This is to make it more obvious to a creator
    that they have successfully added a user (it shows the new user at the
    top).

--- app/shared/MemberTable.jsx
+++ app/shared/MemberTable.jsx
@@ -37,7 +37,7 @@ export default class MemberTable extends Component {
     this.state = {
       sortingColumns: {
         'id': {
-          direction: 'asc',
+          direction: 'desc',
           position: 0,
         },
         'first_name': {
