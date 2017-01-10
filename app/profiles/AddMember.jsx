f4e12daa9b360a0d8cd08b82ab6962e4e07f3899
sdob <sdob@cs.ucc.ie>
2017-01-10 17:43:29 +0000

    Use reusable PersonalDetails

--- app/profiles/AddMember.jsx
+++ app/profiles/AddMember.jsx
@@ -2,14 +2,11 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form';
 import moment from 'moment';
-import { DateTimePicker } from 'react-widgets';
-import momentLocalizer from 'react-widgets/lib/localizers/moment';
 
 import { addMember } from './actions';
 import * as fields from './fields';
 import FormRow from './FormRow';
-
-import 'style!css!react-datepicker/dist/react-datepicker.css';
+import PersonalDetails from './PersonalDetails';
 
 const form = reduxForm({
   form: 'addMember',
@@ -22,66 +19,21 @@ class AddMember extends Component {
     this.handleFormSubmit = this.handleFormSubmit.bind(this);
   }
 
-  DatePicker({ input, meta, value, ...rest }) {
-    return (
-      <DateTimePicker
-        {...input}
-        format="DD/MM/YYYY"
-        value={value}
-        time={false}
-        onBlur={() => input.onBlur(value)}
-        {...rest}
-      />
-   );
-  }
 
   handleFormSubmit(formProps) {
     this.props.addMember(formProps);
   }
 
   render() {
-    momentLocalizer(moment);
-
     const { handleSubmit } = this.props;
+
     return (
       <div className="container">
         <h1 className="sinc-page-header">Add new member</h1>
         <form onSubmit={handleSubmit(this.handleFormSubmit)}>
+
           <h2 className="sinc-section-header">Personal details</h2>
-          <div className="form-group row">
-            <label htmlFor="first_name" className="col-sm-3 col-form-label">First name</label>
-            <div className="col-sm-9">
-              <Field name="first_name" className="form-control" component="input" type="text" />
-            </div>
-          </div>
-
-          <div className="form-group row">
-            <label htmlFor="last_name" className="col-sm-3 col-form-label">Last name</label>
-            <div className="col-sm-9">
-              <Field name="last_name" className="form-control" component="input" type="text" />
-            </div>
-          </div>
-
-          <div className="form-group row">
-            <label htmlFor="gender" className="col-sm-3 col-form-label">Gender</label>
-            <div className="col-sm-4">
-              <select className="form-control">
-                <option>Male</option>
-                <option>Female</option>
-              </select>
-            </div>
-          </div>
-
-
-          <div className="form-group row">
-            <label htmlFor="date_of_birth" className="col-sm-3 col-form-label">Date of birth</label>
-            <div className="col-sm-4">
-              <Field
-                name="date_of_birth"
-                component={this.DatePicker}
-              />
-            </div>
-          </div>
+          <PersonalDetails />
 
           <h2 className="sinc-section-header">Contact details</h2>
 
