43c801196fdae4c5c7cc403a3b75183d0062289e
sdob <sdob@cs.ucc.ie>
2017-01-10 12:47:51 +0000

    Add profile name and link to top nav

--- app/top-nav/TopNav.jsx
+++ app/top-nav/TopNav.jsx
@@ -1,18 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import * as paths from '../paths';
 
 import TopNavItem from './TopNavItem';
 
-export default function TopNav() {
-  return (
-    <nav className="navbar navbar-dark bg-cft-dark sinc-top-nav">
-      <ul className="nav navbar-nav">
-        <TopNavItem to="/Main" icon="home" text="Home" />
-        <TopNavItem to={paths.HELP_PAGE} icon="question" text="Help" />
-        <TopNavItem to={paths.CONTACT_US} icon="envelope" text="Contact Us" />
-        <TopNavItem to={paths.LOGOUT} icon="sign-out" className="float-xs-right" text="Logout" />
-      </ul>
-    </nav>
-  );
+class TopNav extends Component {
+  render() {
+    const { profile } = this.props;
+    return (
+      <nav className="navbar navbar-dark bg-cft-dark sinc-top-nav">
+        <ul className="nav navbar-nav">
+          <TopNavItem to="/Main" icon="home" text="Home" />
+          <TopNavItem to={paths.HELP_PAGE} icon="question" text="Help" />
+          <TopNavItem to={paths.CONTACT_US} icon="envelope" text="Contact Us" />
+          <TopNavItem to={paths.LOGOUT} icon="sign-out" className="float-xs-right" text="Logout" />
+          {profile && (
+            <TopNavItem
+              className="float-xs-right"
+              icon="user"
+              text={`${profile.first_name} ${profile.last_name}`}
+              to={paths.EDIT_PROFILE}
+              />
+          )}
+        </ul>
+      </nav>
+    );
+  }
 }
+
+function mapStateToProps(state) {
+  return {
+    profile: state.profiles.profile,
+  };
+}
+
+export default connect(mapStateToProps)(TopNav);
