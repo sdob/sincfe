022305e007197195d3c0d7c21095b6578c9fe97a
sdob <sdob@cs.ucc.ie>
2017-01-10 16:22:16 +0000

    Fetch region name after profile loads

--- app/active-instructors/ActiveInstructors.jsx
+++ app/active-instructors/ActiveInstructors.jsx
@@ -7,6 +7,7 @@ import * as resolve from 'table-resolver';
 import * as sort from 'sortabular';
 
 import fetchActiveInstructors from './actions';
+import { fetchRegionDetail } from '../regions/actions';
 import PageLoading from '../shared/PageLoading';
 import MemberTable from '../shared/MemberTable';
 
@@ -14,23 +15,25 @@ class ActiveInstructors extends MemberTable {
   componentDidMount() {
     const { profile } = this.props;
     if (profile) {
-      this.props.fetchActiveInstructors(profile.club.region.id);
+      const { region } = profile.club;
+      this.props.fetchActiveInstructors(region);
+      this.props.fetchRegionDetail(region);
     }
   }
 
   componentWillReceiveProps(nextProps) {
     if (nextProps.profile !== this.props.profile) {
-      this.props.fetchActiveInstructors(nextProps.profile.club.region.id);
+      const { region } = nextProps.profile.club;
+      this.props.fetchActiveInstructors(region);
+      this.props.fetchRegionDetail(region);
     }
   }
 
   render() {
-    const { instructors, profile } = this.props;
+    const { instructors, profile, region } = this.props;
     if (!instructors) {
       return <PageLoading />;
     }
-    // TODO: Set this to the appropriate scope (club, region, etc.)
-    const scope = profile.club.region.name;
 
     const rows = instructors;
 
@@ -41,7 +44,7 @@ class ActiveInstructors extends MemberTable {
 
     return (
       <div>
-        <h1>Active Instructors ({scope})</h1>
+        <h1>Active Instructors ({region && region.name})</h1>
         <Table.Provider
           className="table pure-table pure-table-striped"
           columns={columns}
@@ -60,7 +63,8 @@ function mapStateToProps(state) {
   return {
     instructors: state.activeInstructors.instructors,
     profile: state.profiles.profile,
+    region: state.regions.regionDetail,
   };
 }
 
-export default connect(mapStateToProps, { fetchActiveInstructors })(ActiveInstructors);
+export default connect(mapStateToProps, { fetchActiveInstructors, fetchRegionDetail })(ActiveInstructors);
