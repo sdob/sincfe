2a314d847ab61955a0e93ac534b1e4f459705810
sdob <sdob@cs.ucc.ie>
2017-01-10 14:03:34 +0000

    Load fonts and images with bundled CSS

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,9 @@ module.exports = {
       { test: /\.scss$/, loaders: ["style-loader", "css-loader", "sass-loader"] },
       { test: /\.css$/, include: path.resolve(__dirname, 'app'), loader: 'style-loader!css-loader' },
       { test: /\.js[x]?$/, include: path.resolve(__dirname, 'app'), exclude: /node_modules/, loader: 'babel-loader' },
+      { test: /\.gif$/, loader: 'url-loader?mimetype=image/png' }, // Load GIFs
+      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff' },
+      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader?name=[name].[ext]" },
     ]
   },
   resolve: {
