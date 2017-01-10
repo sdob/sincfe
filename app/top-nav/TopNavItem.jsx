e4aa0862d0e88787f3fb017e7ad6e10a798014db
sdob <sdob@cs.ucc.ie>
2017-01-10 12:35:32 +0000

    Clean up nav item class strings

--- app/top-nav/TopNavItem.jsx
+++ app/top-nav/TopNavItem.jsx
@@ -3,14 +3,26 @@ import React from 'react';
 import { Link } from 'react-router';
 
 export default function TopNavItem(props) {
+
+  const { className, icon, text, to } = props;
+  const liClasses = generateClassString(className);
+
   return (
-    <li className={`nav-item ${props.className}`}>
-      <Link to={props.to} className="nav-link" activeClassName="active">
-        {props.icon && (
-          <i className={`fa fa-fw fa-${props.icon}`} />
-        )}
-        <span className="sinc-top-nav__menu-text">{props.text}</span>
+    <li className={liClasses}>
+      <Link to={to} className="nav-link" activeClassName="active">
+        {icon && <i className={`fa fa-fw fa-${icon}`} />}
+        {text && <span className="sinc-top-nav__menu-text">{text}</span>}
       </Link>
     </li>
   );
+}
+
+function generateClassString(className) {
+  // Every <li> gets '.nav-item'; if we were passed a props.className,
+  // then add it too.
+  let classStrings = ['nav-item'];
+  if (typeof className !== 'undefined') {
+    classStrings.push(className);
+  }
+  return classStrings.join(' ');
 }
