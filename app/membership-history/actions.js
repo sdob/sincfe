0209445e701d721f1beae2ce53deab885b3e7248
sdob <sdob@cs.ucc.ie>
2017-01-10 17:12:21 +0000

    Rename event types

--- app/membership-history/actions.js
+++ app/membership-history/actions.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 import { currentMembershipStatusUrl } from '../api';
 
-import { CMS_RECEIVED } from './types';
+import * as types from './types';
 
 export default function fetchCurrentMembershipStatus(uid) {
   return function fetch(dispatch) {
     const url = currentMembershipStatusUrl(uid);
     axios.get(url)
     .then((response) => {
-      dispatch({ type: CMS_RECEIVED, payload: response.data });
+      dispatch({ type: types.MEMBERSHIP_STATUS_RECEIVED, payload: response.data });
     })
     .catch((error) => {
       console.error(error);
--- app/membership-history/reducers.js
+++ app/membership-history/reducers.js
@@ -2,8 +2,10 @@ import * as types from './types';
 
 function currentMembershipStatusReducer(state = {}, action) {
   switch (action.type) {
-    case types.CMS_RECEIVED:
+    case types.MEMBERSHIP_STATUS_RECEIVED:
       return { ...state, currentStatus: action.payload };
+    case types.MEMBERSHIP_STATUS_ERROR:
+      return { ...state, errorMsg: action.payload };
     default:
       return state;
   }
