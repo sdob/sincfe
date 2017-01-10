6195f0b7e558f69231bebec061c5afee2f5630c9
sdob <sdob@cs.ucc.ie>
2017-01-10 19:11:29 +0000

    Listen for profile create actions

--- app/profiles/reducers.js
+++ app/profiles/reducers.js
@@ -6,8 +6,14 @@ import * as types from './types';
 
 export default function authReducer(state = {}, action) {
   switch (action.type) {
+    case types.PROFILE_CREATE_SENDING:
+      return { ...state, sending: true };
+    case types.PROFILE_CREATE_ERROR:
+      return { ...state, errorMsg: action.payload, sending: false };
+    case types.PROFILE_CREATE_SUCCESS:
+      return { ...state, user: action.payload, sending: false };
     case types.PROFILE_RECEIVED:
-      return { ...state, profile: action.payload };
+      return { ...state, profile: action.payload, sending: false };
     default:
       return state;
   }
