940779d2239526d482657d469c6a8417114f3ba7
sdob <sdob@cs.ucc.ie>
2017-01-18 11:21:23 +0000

    Layout Edit club details form

--- app/clubs/ClubDetails.jsx
+++ app/clubs/ClubDetails.jsx
@@ -5,6 +5,9 @@ import PageLoading from '../shared/PageLoading';
 import NotImplementedYet from '../shared/NotImplementedYet';
 import { fetchClub } from './actions';
 import { fetchRegions } from '../regions/actions';
+import FormRow from '../shared/FormRow';
+
+import * as fields from './fields';
 
 const form = reduxForm({
   form: 'editClubDetails',
@@ -14,7 +17,6 @@ class ClubDetails extends Component {
 
   componentDidMount() {
     const { fetchClub, fetchRegions, profile } = this.props;
-    console.info(fetchClub);
     if (profile && profile.club) {
       fetchClub(profile.club.id);
       fetchRegions();
@@ -35,9 +37,10 @@ class ClubDetails extends Component {
       return <PageLoading />;
     }
 
-    console.info(regions);
-
+    // Try to find a region for this club; if it's missing, call it
+    // 'None assigned'
     const region = regions.filter(region => region.id === club.region)[0];
+    const regionName = region ? region.name : 'None assigned';
 
     return (
       <div>
@@ -59,13 +62,40 @@ class ClubDetails extends Component {
           </label>
           <div className="col-xs-6 col-md-9">
             <p className="form-control-static">
-              {region.name}
+              {regionName}
             </p>
           </div>
         </div>
 
+        <FormRow field={fields.DESCRIPTION} label="Description" component="textarea" rows="3" />
+        <FormRow field={fields.CONTACT_NAME} label="Contact name" />
+        <FormRow field={fields.CONTACT_EMAIL} label="Email address" />
+        <FormRow field={fields.CONTACT_PHONE} label="Phone number" />
+        <FormRow field={fields.LOCATION} label="Location" component="textarea" rows="3" />
+        <FormRow field={fields.TRAINING_TIMES} label="Training times" component="textarea" rows="3" />
+
+        <div className="row">
+          <div className="col-md-9 col-lg-6 offset-md-3 sinc-form__submit-row">
+            <button
+              className="btn btn-primary"
+              type="button"
+            >
+              Save
+            </button>
+          </div>
+        </div>
+
+      </div>
+    );
+
+    /*
+
+    return (
+      <div>
+
+
         <div className="form-group row">
-          <label className="col-xs-12 col-md-3 col-form-label">
+          <label htmlFor={fields.DESCRIPTION} className="col-xs-12 col-md-3 col-form-label">
             Description
           </label>
           <div className="col-xs-12 col-md-9">
@@ -81,8 +111,27 @@ class ClubDetails extends Component {
             <Field name='contactName' component="input" className="form-control" />
           </div>
         </div>
+
+        <div className="form-group row">
+          <label className="col-xs-12 col-md-3 col-form-label">
+            Contact email
+          </label>
+          <div className="col-xs-12 col-md-9">
+            <Field name='contactName' component="input" className="form-control" />
+          </div>
+        </div>
+
+        <div className="form-group row">
+          <label className="col-xs-12 col-md-3 col-form-label">
+            Contact phone
+          </label>
+          <div className="col-xs-12 col-md-9">
+            <Field name='contactName' component="input" className="form-control" />
+          </div>
+        </div>
       </div>
     );
+    */
   }
 }
 
