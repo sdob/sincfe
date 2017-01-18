f1dd8257338224cdc3ab310a53a1dbf398307876
sdob <sdob@cs.ucc.ie>
2017-01-18 11:50:40 +0000

    Send data to API server

--- app/clubs/actions.js
+++ app/clubs/actions.js
@@ -17,15 +17,19 @@ function fetchClub(cid) {
 }
 
 function updateClub(club) {
-  const { id } = club;
   return function update(dispatch) {
+    const { id } = club;
     const url = clubDetailUrl(id);
-    console.info(`sending to: ${url}`);
     dispatch({ type: types.CLUB_DETAIL_SENDING });
-    // TODO: actually submit the data
-    setTimeout(function() {
-      dispatch({ type: types.CLUB_DETAIL_RECEIVED, payload: club });
-    }, 500);
+    axios.put(url, club)
+    .then((response) => {
+      const { data } = response;
+      dispatch({ type: types.CLUB_DETAIL_RECEIVED, payload: data });
+    })
+    .catch((error) => {
+      console.error(error);
+      throw error;
+    });
   }
 }
 
