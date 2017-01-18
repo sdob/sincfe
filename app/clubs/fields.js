a973d704d62cfc8b67c49b840cd0200be8a1b8ed
sdob <sdob@cs.ucc.ie>
2017-01-18 11:01:38 +0000

    Add field constants

---
+++ app/clubs/fields.js
@@ -0,0 +1,9 @@
+export const NAME = 'name';
+export const REGION = 'region';
+export const DESCRIPTION = 'description';
+export const CONTACT_NAME = 'contact_name';
+export const CONTACT_EMAIL = 'contact_email';
+export const CONTACT_PHONE = 'contact_phone';
+export const FOUNDATION_DATE = 'foundation_date';
+export const LOCATION = 'location';
+export const TRAINING_TIMES = 'training_times';
