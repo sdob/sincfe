331411a81ea2ef83619a30d92013dff8b0725bc9
sdob <sdob@cs.ucc.ie>
2017-01-19 11:32:08 +0000

    Add profile update reducer cases

--- app/profiles/reducers.js
+++ app/profiles/reducers.js
@@ -4,18 +4,27 @@ import * as types from './types';
  * Handle profile-related actions here
  */
 
-export default function authReducer(state = {}, action) {
+export default function authReducer(initialState = {}, action) {
+  // Usually, we want to make sure that 'sending' is false
+  const state = { ...initialState, sending: false };
   switch (action.type) {
     case types.PROFILE_CREATE_SENDING:
       return { ...state, sending: true };
     case types.PROFILE_CREATE_ERROR:
-      return { ...state, errorMsg: action.payload, sending: false };
+      return { ...state, errorMsg: action.payload };
     case types.PROFILE_CREATE_SUCCESS:
-      return { ...state, user: action.payload, sending: false };
+      return { ...state, user: action.payload };
     case types.PROFILE_RECEIVED:
-      return { ...state, profile: action.payload, sending: false };
+      return { ...state, profile: action.payload };
+    // Profile updates
+    case types.PROFILE_UPDATE_SENDING:
+      return { ...state, sending: true };
+    case types.PROFILE_UPDATE_SENDING:
+      return { ...state, profile: action.payload };
+    case types.PROFILE_UPDATE_ERROR:
+      return { ...state, errorMsg: action.payload };
     default:
-      return state;
+      return initialState;
   }
 }
 
