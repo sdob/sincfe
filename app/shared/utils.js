ec29121ae2aeec107217f30d21a82e9504e676b8
sdob <sdob@cs.ucc.ie>
2017-01-20 12:19:25 +0000

    Use default export (prefer-default-export)

--- app/shared/utils.js
+++ app/shared/utils.js
@@ -4,10 +4,6 @@ import moment from 'moment';
  * Helper function for formatting dates of birth to meet Django's
  * expectations
  */
-function formatDate(date) {
+export default function formatDate(date) {
   return moment(date).format('YYYY-MM-DD');
 }
-
-export {
-  formatDate,
-};
