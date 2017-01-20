785ad5a8bc61f0d9394042ade2430efcf4b8c9ac
sdob <sdob@cs.ucc.ie>
2017-01-20 12:15:05 +0000

    Add title form field to PersonalDetails

--- app/profiles/PersonalDetails.jsx
+++ app/profiles/PersonalDetails.jsx
@@ -3,11 +3,32 @@ import { Field } from 'redux-form';
 import DatePicker from '../shared/DatePicker';
 import * as fields from './fields';
 import * as genders from './genders';
+import * as titles from './titles';
 import FormRow from '../shared/FormRow';
 
 export default function PersonalDetails() {
   return (
     <div>
+
+      <div className="form-group row">
+        <label
+          htmlFor="editProfileTitle"
+          className="col-sm-6 col-md-3 col-form-label"
+        >
+          Title
+        </label>
+        <div className="col-sm-6 col-md-4 col-lg-3">
+          <Field name={fields.TITLE} component="select" className="form-control">
+            <option value="-1">Select title</option>
+            <option value={titles.DR}>Dr</option>
+            <option value={titles.MISS}>Miss</option>
+            <option value={titles.MR}>Mr</option>
+            <option value={titles.MRS}>Mrs</option>
+            <option value={titles.MS}>Ms</option>
+          </Field>
+        </div>
+      </div>
+
       <FormRow field={fields.FIRST_NAME} label="First name" />
       <FormRow field={fields.LAST_NAME} label="Last name" />
 
