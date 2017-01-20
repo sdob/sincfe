e19fb70678e5848da7a80a22849cc7cd6d2d663c
sdob <sdob@cs.ucc.ie>
2017-01-20 12:36:36 +0000

    Fix indentation issues

--- app/notifications/NotificationContainer.jsx
+++ app/notifications/NotificationContainer.jsx
@@ -6,24 +6,24 @@ import * as profileTypes from '../profiles/types';
 export default function NotificationContainer() {
   return (
     <div className="sinc-notification-container">
-        <FlashNotification
-          hideAfter={2000}
-          message="Changes saved!"
-          status="success"
-          trigger={clubTypes.CLUB_UPDATE_SUCCESS}
-        />
-        <FlashNotification
-          hideAfter={2000}
-          message="Profile updated!"
-          status="success"
-          trigger={profileTypes.PROFILE_UPDATE_SUCCESS}
-        />
-        <FlashNotification
-          hideAfter={2000}
-          message="Member details updated!"
-          status="success"
-          trigger={profileTypes.UPDATE_MEMBER_SUCCESS}
-        />
+      <FlashNotification
+        hideAfter={2000}
+        message="Changes saved!"
+        status="success"
+        trigger={clubTypes.CLUB_UPDATE_SUCCESS}
+      />
+      <FlashNotification
+        hideAfter={2000}
+        message="Profile updated!"
+        status="success"
+        trigger={profileTypes.PROFILE_UPDATE_SUCCESS}
+      />
+      <FlashNotification
+        hideAfter={2000}
+        message="Member details updated!"
+        status="success"
+        trigger={profileTypes.UPDATE_MEMBER_SUCCESS}
+      />
     </div>
   );
 }
