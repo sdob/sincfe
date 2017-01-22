96fc7c622e016608261e970dcda0116e0daea61a
sdob <sdob@cs.ucc.ie>
2017-01-22 15:10:56 +0000

    Extract loader rules to their own file

---
+++ config/rules.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = function(dirname) {
+  return [
+    { test: /\.scss$/, loaders: [ "style-loader", "css-loader", "sass-loader" ] },
+    { test: /\.css$/, include: path.resolve(dirname, 'app'), loaders: [ 'style-loader', 'css-loader' ] },
+    { test: /\.js[x]?$/, include: path.resolve(dirname, 'app'), exclude: /node_modules/, loader: 'babel-loader' },
+    { test: /\.gif$/, loader: 'url-loader?mimetype=image/png' }, // Load GIFs
+    { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff' },
+    { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader?name=[name].[ext]" },
+  ];
+
+};
