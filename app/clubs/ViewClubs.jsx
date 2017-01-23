deb776002bb03770e0c78ac021c5769e3d3ea80e
sdob <sdob@cs.ucc.ie>
2017-01-23 12:59:58 +0000

    Fetch club list when ViewClubs has mounted

--- app/clubs/ViewClubs.jsx
+++ app/clubs/ViewClubs.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import NotImplementedYet from '../shared/NotImplementedYet';
+import { fetchClubList } from './actions';
 
-export default function ViewClubs() {
-  return (
-    <div>
-      <h1 className="sinc-page-header">View clubs</h1>
-      <NotImplementedYet />
-    </div>
-  );
+class ViewClubs extends Component {
+  componentDidMount() {
+    this.props.fetchClubList();
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 className="sinc-page-header">View clubs</h1>
+        <NotImplementedYet />
+      </div>
+    );
+  }
 }
+
+function mapStateToProps(state) {
+  return {
+    clubs: state.clubs.clubList,
+  };
+}
+
+export default connect(mapStateToProps, { fetchClubList })(ViewClubs);
