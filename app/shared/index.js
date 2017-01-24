23f9f0752c71aae3efd1a8565c0aa64487ff567e
sdob <sdob@cs.ucc.ie>
2017-01-24 16:07:59 +0000

    Add default module export for shared

---
+++ app/shared/index.js
@@ -0,0 +1,25 @@
+import CertificateSelector from './CertificateSelector';
+import DatePicker from './DatePicker';
+import FormRow from './FormRow';
+import GenericErrorMessage from './GenericErrorMessage';
+import InlineSpinner from './InlineSpinner';
+import MemberTable from './MemberTable';
+import NotImplementedYet from './NotImplementedYet';
+import PageLoading from './PageLoading';
+import SortedTable from './SortedTable';
+import SubmitRow from './SubmitRow';
+import UneditableProfileHelp from './UneditableProfileHelp';
+
+export {
+  CertificateSelector,
+  DatePicker,
+  FormRow,
+  GenericErrorMessage,
+  InlineSpinner,
+  MemberTable,
+  NotImplementedYet,
+  PageLoading,
+  SortedTable,
+  SubmitRow,
+  UneditableProfileHelp,
+};
