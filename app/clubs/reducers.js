bb69db25b5c18060a7d86a81ca49ee9604dcba62
sdob <sdob@cs.ucc.ie>
2017-01-25 16:19:55 +0000

    Distinguish between GET and UPDATE success

--- app/clubs/reducers.js
+++ app/clubs/reducers.js
@@ -8,7 +8,7 @@ export default function clubsReducer(initialState = {}, action) {
     case types.CLUB_DETAIL_RECEIVED:
       return { ...state, club: action.payload };
     case types.CLUB_UPDATE_SUCCESS:
-      return { ...state, club: action.payload };
+      return { ...state, update: action.payload };
     case types.MEMBER_LIST_RECEIVED:
       return { ...state, memberList: action.payload };
     case types.CLUB_LIST_RECEIVED:
