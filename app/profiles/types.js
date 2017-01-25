74214812f3b906bd347acb0a98b60a646a67fffc
sdob <sdob@cs.ucc.ie>
2017-01-25 15:58:29 +0000

    Add createApiAction-compatible type objects

--- app/profiles/types.js
+++ app/profiles/types.js
@@ -1,4 +1,7 @@
-export const PROFILE_RECEIVED = 'PROFILE_RECEIVED';
+export const OWN_PROFILE_RECEIVED = 'OWN_PROFILE_RECEIVED';
+export const OWN_PROFILE_FETCHING = 'OWN_PROFILE_FETCHING';
+export const OWN_PROFILE_ERROR = 'OWN_PROFILE_ERROR';
+
 export const PROFILE_CREATE_SENDING = 'PROFILE_CREATE_SENDING';
 export const PROFILE_CREATE_SUCCESS = 'PROFILE_CREATE_SUCCESS';
 export const PROFILE_CREATE_ERROR = 'PROFILE_CREATE_ERROR';
@@ -18,3 +21,48 @@ export const MEMBER_LIST_ERROR = 'MEMBER_LIST_ERROR';
 export const UPDATE_MEMBER_SENDING = 'UPDATE_MEMBER_SENDING';
 export const UPDATE_MEMBER_SUCCESS = 'UPDATE_MEMBER_SUCCESS';
 export const UPDATE_MEMBER_ERROR = 'UPDATE_MEMBER_ERROR';
+
+const memberCreate = {
+  pending: PROFILE_CREATE_SENDING,
+  success: PROFILE_CREATE_SUCCESS,
+  error: PROFILE_CREATE_ERROR,
+};
+
+const memberDetail = {
+  pending: MEMBER_DETAIL_FETCHING,
+  success: MEMBER_DETAIL_RECEIVED,
+  error: MEMBER_LIST_ERROR,
+};
+
+const memberList = {
+  pending: MEMBER_LIST_FETCHING,
+  success: MEMBER_LIST_RECEIVED,
+  error: MEMBER_LIST_ERROR,
+};
+
+const ownProfileRetrieve = {
+  pending: OWN_PROFILE_FETCHING,
+  success: OWN_PROFILE_RECEIVED,
+  error: OWN_PROFILE_ERROR,
+};
+
+const memberUpdate = {
+  pending: 'UPDATE_MEMBER_SENDING',
+  success: 'UPDATE_MEMBER_SUCCESS',
+  error: 'UPDATE_MEMBER_ERROR',
+}
+
+const ownProfileUpdate = {
+  pending: PROFILE_UPDATE_SENDING,
+  success: PROFILE_UPDATE_SUCCESS,
+  error: PROFILE_UPDATE_ERROR,
+};
+
+export {
+  memberCreate,
+  memberDetail,
+  memberList,
+  memberUpdate,
+  ownProfileRetrieve,
+  ownProfileUpdate,
+};
