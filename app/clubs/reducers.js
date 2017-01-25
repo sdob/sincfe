def9a59463d746ab3419d08a9ec385b612968bbd
sdob <sdob@cs.ucc.ie>
2017-01-25 16:23:52 +0000

    Send 'sending' event when update starts

--- app/clubs/reducers.js
+++ app/clubs/reducers.js
@@ -1,5 +1,7 @@
 import * as types from './types';
 
+const { clubUpdate } = types;
+
 export default function clubsReducer(initialState = {}, action) {
   const state = { ...initialState, sending: false };
   switch (action.type) {
@@ -7,6 +9,8 @@ export default function clubsReducer(initialState = {}, action) {
       return { ...state, sending: true };
     case types.CLUB_DETAIL_RECEIVED:
       return { ...state, club: action.payload };
+    case clubUpdate.pending:
+      return { ...state, sending: true };
     case types.CLUB_UPDATE_SUCCESS:
       return { ...state, update: action.payload };
     case types.MEMBER_LIST_RECEIVED:
