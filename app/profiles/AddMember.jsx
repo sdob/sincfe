db541f6df17ac5352d33b1cef5766a2f00e8a9d0
sdob <sdob@cs.ucc.ie>
2017-01-27 16:48:37 +0000

    Validate AddMember

--- app/profiles/AddMember.jsx
+++ app/profiles/AddMember.jsx
@@ -7,10 +7,12 @@ import { FormRow, InlineSpinner } from '../shared';
 import formatDate from '../shared/utils';
 import { addMember } from './actions';
 import * as fields from './fields';
+import ContactDetails from './ContactDetails';
 import PersonalDetails from './PersonalDetails';
 
 const form = reduxForm({
   form: 'addMember',
+  validate,
 });
 
 class AddMember extends Component {
@@ -31,7 +33,9 @@ class AddMember extends Component {
 
   handleFormSubmit(formProps) {
     const user = { ...formProps, date_of_birth: formatDate(formProps.date_of_birth) };
-    this.props.addMember(user);
+    console.info('should send...');
+    console.info(user);
+    // this.props.addMember(user);
   }
 
   render() {
@@ -46,13 +50,7 @@ class AddMember extends Component {
           <PersonalDetails />
 
           <h2 className="sinc-section-header">Contact details</h2>
-
-          <FormRow field={fields.EMAIL} label="Email" />
-          <FormRow field={fields.PHONE_HOME} label="Phone (home)" />
-          <FormRow field={fields.PHONE_MOBILE} label="Phone (mobile)" />
-          <FormRow field={fields.ADDRESS} component="textarea" rows="3" label="Address" />
-          <FormRow field={fields.NEXT_OF_KIN_NAME} label="Next of kin" />
-          <FormRow field={fields.NEXT_OF_KIN_PHONE} label="Next of kin (phone)" />
+          <ContactDetails />
 
           <h2 className="sinc-section-header">Mailing</h2>
 
@@ -91,6 +89,32 @@ function mapStateToProps(state) {
   return {
     profiles: state.profiles,
   };
+}
+
+function validate(values) {
+  const errors = {};
+  const requiredFields = [
+    fields.TITLE,
+    fields.FIRST_NAME,
+    fields.LAST_NAME,
+    fields.GENDER,
+    fields.DATE_OF_BIRTH,
+    fields.EMAIL,
+  ];
+  requiredFields.forEach((field) => {
+    if (!values[field]) {
+      errors[field] = 'This field may not be blank.';
+    }
+  });
+
+  // Handle selects
+  const requiredSelects = [fields.TITLE, fields.GENDER];
+  requiredSelects.forEach((field) => {
+    if (!values[field] || values[field] < 0) {
+      errors[field] = 'This field may not be blank.';
+    }
+  });
+  return errors;
 }
 
 export default connect(mapStateToProps, { addMember })(form(AddMember));
