808ebfc5a921c2a68cf407e2e6a4e1b3a53319dd
sdob <sdob@cs.ucc.ie>
2017-01-27 16:49:12 +0000

    Refactor FormRow

--- app/shared/FormRow.jsx
+++ app/shared/FormRow.jsx
@@ -8,45 +8,35 @@ import { Field } from 'redux-form';
  * down on boilerplate in other components.
  */
 export default function FormRow(props) {
-  const { component, field, label, required, rows } = props;
+  const { field, required } = props;
+  return ( <Field name={field} {...props} component={renderField} />
+  );
+}
+
+function renderField({ component, field, input, label, required, rows, type, meta: { touched, error } }) {
   return (
-    <div className="form-group row">
-      <label htmlFor={field} className="col-md-3 col-form-label">
-        {label}
-      </label>
-      <div className="col-md-9 col-lg-6">
-        {required && <div className="form-text text-muted">Required.</div>}
-        <Field
-          name={field}
-          component={component === 'textarea' ? renderTextArea : renderInput}
-          rows={rows}
-          type="text"
-        />
+    <div>
+      <div className={`form-group row${(touched && error) ? ' has-danger' : ''}`}>
+        <div className="col-md-3 col-form-label">
+          <label htmlFor={field} className="form-control-label">
+            {label}
+          </label>
+        </div>
+        <div className="col-md-9 col-lg-6">
+          {type === 'textarea' ? renderTextArea({ field, input, required, rows}) : renderInput({ field, input, required, type })}
+          {touched && error && <ErrorMessage error={error} />}
+        </div>
       </div>
     </div>
   );
 }
 
-function renderInput({ input, type, meta: { touched, error } }) {
-  let className="form-control";
-  if (error) {
-    className = `${className} text-danger}`;
-  }
-  return (
-    <div className={touched && error && 'has-danger'}>
-      {touched && error && <ErrorMessage error={error} />}
-      <input {...input} className={className} type={type} />
-    </div>
-  );
+function renderInput({ field, input, required, type }) {
+  return <input {...input} name={field} className="form-control" placeholder={required ? '' : 'Optional'} type={type} />
 }
 
-function renderTextArea({ rows, meta: { touched, error } }) {
-  return (
-    <div>
-      {touched && error && <ErrorMessage error={error} />}
-      <textarea className="form-control" rows={rows} />
-    </div>
-  );
+function renderTextArea({ field, input, rows }) {
+  return <textarea {...input} name={field} className="form-control" rows={rows} />;
 }
 
 function ErrorMessage({ error }) {
