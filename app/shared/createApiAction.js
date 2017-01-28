b1c225a555bac48ca95016e877a140ba3643dd01
sdob <sdob@cs.ucc.ie>
2017-01-28 13:15:10 +0000

    Use an object instead of ordered params when creating API action

--- app/shared/createApiAction.js
+++ app/shared/createApiAction.js
@@ -5,27 +5,37 @@ import logoutUser from '../auth/actions';
 /*
  * This is a factory method that creates Redux actions that contact the
  * API (since a lot of the behaviour is shared between actions). Pass
- * it a URL, a method ('get', 'post', etc.), an 'types' object, and
- * (optionally) form data to be sent.
- *
- * The 'types' object should have 'pending', 'success', and 'error'
- * keys, which will be dispatched, respectively,
- * (1) when the API call is sent;
- * (2) when a successful response is returned; and
- * (3) when an error is returned.
- *
- * The returned action will handle HTTP 401 Unauthorized errors
- * automatically, since this type of error is a special case that
- * just requires us to log the user out.
+ * in an object containing the following:
+ * url: the URL of the endpoint,
+ * method: 'get', 'post', etc.,
+ * types: an object containing events to be dispatched, in the format
+ *    { pending: <string>, success: <string>, error: <string> },
+ * formatRequest (optional): a function that transforms the request data
+ *    before it's sent,
+ * formatResponse (optional): a function that transforms the response data
+ *    when it's received but before we dispatch it to the reducers.
  */
-export default function createApiAction(url, method, types, data) {
+export default function createApiAction({
+  url,
+  method,
+  types,
+  data,
+  formatRequest,
+  formatResponse,
+}) {
   const { pending, success, error } = types;
+  // TODO: It seems like it ought to be possible to declare these identity
+  // functions as default parameters, but I can't get that to work.
+  const _formatRequest = formatRequest ? formatRequest : function(x) {return x;};
+  const _formatResponse = formatResponse ? formatResponse : function(x) {return x;};
   return (dispatch) => {
     dispatch({ type: pending });
-    return axios[method](url, data)
+    return axios[method](url, _formatRequest(data))
+    .then(formatResponse)
     .then((response) => {
       const { data } = response;
       dispatch({ type: success, payload: data });
+      return data;
     })
     .catch((err) => {
       // Special case: HTTP 401 Unauthorized. This should happen very
