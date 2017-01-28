a9ffe589b7a804f5c9de291cb65a2ca98d871ee0
sdob <sdob@cs.ucc.ie>
2017-01-28 13:13:19 +0000

    Add reusable member form validation

---
+++ app/profiles/validate.js
@@ -0,0 +1,27 @@
+import * as fields from './fields';
+
+export default function validate(values) {
+  const errors = {};
+  const requiredFields = [
+    fields.TITLE,
+    fields.FIRST_NAME,
+    fields.LAST_NAME,
+    fields.GENDER,
+    fields.DATE_OF_BIRTH,
+    fields.EMAIL,
+  ];
+  requiredFields.forEach((field) => {
+    if (!values[field]) {
+      errors[field] = 'This field may not be blank.';
+    }
+  });
+
+  // Handle selects
+  const requiredSelects = [fields.TITLE, fields.GENDER];
+  requiredSelects.forEach((field) => {
+    if (!values[field] || values[field] < 0) {
+      errors[field] = 'This field may not be blank.';
+    }
+  });
+  return errors;
+}
