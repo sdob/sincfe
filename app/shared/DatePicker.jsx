0181fb052db2013f398a48e96fd4efdc6b12cfcb
sdob <sdob@cs.ucc.ie>
2017-01-28 13:15:52 +0000

    Use the correct value when rendering DatePicker

--- app/shared/DatePicker.jsx
+++ app/shared/DatePicker.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { DateTimePicker } from 'react-widgets';
 
 // eslint-disable-next-line no-unused-vars
-export default function DatePicker({ input, meta, value, ...rest }) {
+export default function DatePicker({ input, meta,  ...rest }) {
+  const { name, value } = input;
   return (
     <DateTimePicker
       {...input}
       format="DD/MM/YYYY"
-      value={value}
       time={false}
+      value={new Date((value || null))}
       onBlur={() => input.onBlur(value)}
       {...rest}
     />
