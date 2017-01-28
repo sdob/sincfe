442386e25d3d0a0c2ace5a03171e2cb8fd826563
sdob <sdob@cs.ucc.ie>
2017-01-28 13:27:34 +0000

    Pass object to createApiAction

--- app/profiles/actions.js
+++ app/profiles/actions.js
@@ -1,8 +1,11 @@
 import axios from 'axios';
 import HTTP from 'http-status-codes';
+import moment from 'moment';
 
 import { addMemberUrl, memberDetailUrl, memberListUrl, ownProfileUrl } from '../api';
 import { createApiAction } from '../shared';
+// import formatDate from '../shared/utils';
+import { date2django, django2date } from '../shared/dateUtils';
 import { logoutUser } from '../auth/actions';
 import * as types from './types';
 import {
@@ -15,22 +18,53 @@ import {
 } from './types';
 
 /* Create a new user. */
-const addMember = (user) => createApiAction(addMemberUrl(), 'post', memberCreate, user);
+const addMember = (user) => createApiAction({
+  url: addMemberUrl(),
+  method: 'post',
+  types: memberCreate,
+  data: user,
+  formatRequest: formatUserDOB,
+});
 
 /* Retrieve a user's profile by ID. */
-const fetchMember = (uid) => createApiAction(memberDetailUrl(uid), 'get', memberDetail);
+const fetchMember = (uid) => createApiAction({
+  url: memberDetailUrl(uid),
+  method: 'get',
+  types: memberDetail,
+});
 
 /* Retrieve all SINC members (which will be a *lot*, so use sparingly) */
-const fetchMembers = () => createApiAction(memberListUrl(), 'get', memberList);
+const fetchMembers = () => createApiAction({
+  url: memberListUrl(), method: 'get', types: memberList,
+});
 
 /* Retrieve the user's profile information. */
-const fetchProfile = () => createApiAction(ownProfileUrl(), 'get', ownProfileRetrieve);
+const fetchProfile = () => createApiAction({
+  url: ownProfileUrl(),
+  method: 'get',
+  types: ownProfileRetrieve,
+});
 
-/* Update a user. */
-const updateMember = (user) => createApiAction(memberDetailUrl(user.id), 'post', memberUpdate, user);
+/* Update a user. Convert the Date object to a string that Django accepts before sending.  */
+const updateMember = (user) => createApiAction({
+  url: memberDetailUrl(user.id),
+  method: 'patch',
+  types: memberUpdate, data: user,
+  formatRequest: formatUserDOB,
+});
 
 /* Update the user's own profile. */
-const updateOwnProfile = (user) => createApiAction(memberDetailUrl(user.id), 'patch', ownProfileUpdate, user);
+const updateOwnProfile = (user) => createApiAction({
+  url: memberDetailUrl(user.id),
+  method: 'patch',
+  types: ownProfileUpdate,
+  data: user,
+  formatRequest: formatUserDOB,
+});
+
+function formatUserDOB(user) {
+  return { ...user, date_of_birth: date2django(user.date_of_birth) };
+}
 
 export {
   addMember,
