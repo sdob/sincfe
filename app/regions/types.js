90000d4a0d0510b1568109a5cc8e9e2a34591b43
sdob <sdob@cs.ucc.ie>
2017-01-31 12:32:05 +0000

    Remove commented-out action consts

--- app/regions/types.js
+++ app/regions/types.js
@@ -7,9 +7,3 @@ export {
   regionDetail,
   regionList,
 };
-
-/*
-export const REGIONS_RECEIVED = 'REGIONS_RECEIVED';
-export const REGIONS_ERROR = 'REGIONS_ERROR';
-export const REGION_DETAIL_RECEIVED = 'REGION_DETAIL_RECEIVED';
-*/
