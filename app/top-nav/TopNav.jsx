ed3019143b409ac7f72f1fe719cc29ee944bda16
sdob <sdob@cs.ucc.ie>
2017-02-08 17:18:11 +0000

    Update BS classes

--- app/top-nav/TopNav.jsx
+++ app/top-nav/TopNav.jsx
@@ -8,22 +8,38 @@ import TopNavItem from './TopNavItem';
 function TopNav(props) {
   const { profile } = props;
   return (
-    <nav className="navbar navbar-dark bg-cft-dark sinc-top-nav">
-      <ul className="nav navbar-nav">
-        <TopNavItem to="/Main" icon="home" text="Home" />
-        <TopNavItem to={paths.HELP_PAGE} icon="question" text="Help" />
-        <TopNavItem to={paths.CONTACT_US} icon="envelope" text="Contact Us" />
-        <TopNavItem to={paths.LOGOUT} icon="sign-out" className="float-xs-right" text="Logout" />
-        {profile && (
-          <TopNavItem
-            className="float-xs-right"
-            icon="user"
-            text={`${profile.first_name} ${profile.last_name}`}
-            to={paths.EDIT_PROFILE}
-          />
-        )}
-      </ul>
-    </nav>
+    <nav className="navbar navbar-toggleable-lg navbar-inverse sinc-top-nav">
+      <button
+        aria-controls="navbarNavDropdown"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+        className="navbar-toggler"
+        data-target="#js-navbar-nav-dropdown"
+        data-toggle="collapse"
+        type="button"
+      >
+        <i className="fa fa-fw fa-bars" />
+      </button>
+      <div className="collapse navbar-collapse" id="js-navbar-nav-dropdown">
+        <ul className="navbar-nav">
+          <TopNavItem to="/Main" icon="home" text="Home" />
+          <TopNavItem to={paths.HELP_PAGE} icon="question" text="Help" />
+          <TopNavItem to={paths.CONTACT_US} icon="envelope" text="Contact Us" />
+        </ul>
+        <div className="justify-content-md-end">
+          <ul className="navbar-nav">
+            {profile && (
+              <TopNavItem
+                icon="user"
+                text={`${profile.first_name} ${profile.last_name}`}
+                to={paths.EDIT_PROFILE}
+              />
+             )}
+           <TopNavItem to={paths.LOGOUT} icon="sign-out" text="Logout" />
+        </ul>
+      </div>
+    </div>
+  </nav>
   );
 }
 
--- app/top-nav/TopNavItem.jsx
+++ app/top-nav/TopNavItem.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 import { Link } from 'react-router';
 
 export default function TopNavItem(props) {
-  const { className, icon, text, to } = props;
-  const liClasses = generateClassString(className);
+  const { icon, text, to } = props;
+  const className = generateClassName(props.className);
 
   return (
-    <li className={liClasses}>
+    <li className={className}>
       <Link to={to} className="nav-link" activeClassName="active">
         {icon && <i className={`fa fa-fw fa-${icon}`} />}
         {text && <span className="sinc-top-nav__menu-text">{text}</span>}
@@ -16,12 +16,12 @@ export default function TopNavItem(props) {
   );
 }
 
-function generateClassString(className) {
+function generateClassName(className) {
   // Every <li> gets '.nav-item'; if we were passed a props.className,
   // then add it too.
-  const classStrings = ['nav-item'];
+  const base = ['nav-item'];
   if (typeof className !== 'undefined') {
-    classStrings.push(className);
+    base.push(className);
   }
-  return classStrings.join(' ');
+  return base.join(' ');
 }
