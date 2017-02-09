3f83c4ad805106c3167565fc495d680d401dc24d
sdob <sdob@cs.ucc.ie>
2017-02-09 11:14:08 +0000

    Fix column definitions

--- app/shared/SelectRow.jsx
+++ app/shared/SelectRow.jsx
@@ -12,17 +12,17 @@ function renderSelect({ field, input, label, options, meta: { touched, error } }
   const className = `form-group row${(touched && error) ? ' has-danger' : ''}`;
   return (
     <div className={className}>
-      <div className="col-xs-12 col-sm-6 col-md-4 col-form-label">
+      <div className="col-12 col-sm-6 col-md-3 col-form-label">
         <label htmlFor={field} className="form-control-label">
           {label}
         </label>
       </div>
-      <div className="col-xs-12 col-sm-6 col-md-8 col-lg-3">
+      <div className="col-12 col-sm-6 col-md-9 col-lg-3">
         <select {...input} className="form-control">
           {options.map(renderOption)}
         </select>
       </div>
-      <div className="col-xs-12 col-sm-6 offset-sm-6 col-md-9 offset-md-4">
+      <div className="col-12 col-sm-6 offset-sm-6 col-md-9 offset-md-3">
         {touched && error && <div className="form-control-feedback text-danger">{error}</div>}
       </div>
     </div>
