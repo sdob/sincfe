173d5fe980ed42d98afdb86d62c2710efc7da8b2
sdob <sdob@cs.ucc.ie>
2017-02-14 15:57:45 +0000

    Parse member roles

--- app/profiles/index.js
+++ app/profiles/index.js
@@ -1,5 +1,9 @@
 import { fetchProfile } from './actions';
+import getMemberRoles from './get-member-roles';
+import * as profileRoles from './profile-roles';
 
 export {
   fetchProfile,
+  getMemberRoles,
+  profileRoles,
 };
