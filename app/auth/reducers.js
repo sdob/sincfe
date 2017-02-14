ebb57ecbf0ebd1d2ca899a70b7e1e4f720d1eac7
sdob <sdob@cs.ucc.ie>
2017-02-14 15:14:52 +0000

    Delete state on logout

--- app/auth/reducers.js
+++ app/auth/reducers.js
@@ -12,20 +12,19 @@ function authReducer(state = INITIAL_STATE, action) {
     case types.LOGIN_SUCCESS:
       return { ...state, error: '', message: '', authenticated: true };
 
-    // LOGOUT_SUCCESS means that the user has successfully logged out;
-    // set state.authenticated to false
-    case types.LOGOUT_SUCCESS:
-      return { ...state, authenticated: false };
-
     // LOGIN_FAILURE means that there was a problem logging the user in
     // (user error or server error); add the error to state
     case types.LOGIN_FAILURE:
       return { ...state, error: action.payload };
 
+    // LOGOUT_USER means that the user has successfully logged out;
+    // delete all state
+    case types.LOGOUT_USER:
+      return INITIAL_STATE;
+
     // PROFILE_RECEIVED means that the user's profile info was successfully
     // received; add it to state
     case types.PROFILE_RECEIVED: // The user's info was successfully received
-      console.log('profile received!');
       return { ...state, profile: action.payload };
 
     // Default case: just return the state as-is
