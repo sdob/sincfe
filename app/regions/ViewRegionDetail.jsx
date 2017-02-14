5c8813bc9afc786fe5f28f0532a69c7f5b499478
sdob <sdob@cs.ucc.ie>
2017-02-14 12:35:37 +0000

    Add region detail view

---
+++ app/regions/ViewRegionDetail.jsx
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import {
+  MemberTable,
+  PageLoading,
+} from '../shared';
+import { fetchRegionClubList, fetchRegionDetail, fetchRegionMemberList } from './actions';
+
+class ViewRegionDetail extends Component {
+  componentDidMount() {
+    const regionId = this.context.router.params.id;
+    this.props.fetchRegionClubList(regionId);
+    this.props.fetchRegionDetail(regionId);
+    this.props.fetchRegionMemberList(regionId);
+  }
+
+  render() {
+    const { clubs, members, region } = this.props;
+    return (
+      <div>
+        <h1 className="sinc-page-header">
+          View region
+          {region === undefined ? '' : `: ${region.name}`}
+        </h1>
+        <h2 className="sinc-section-header">
+          Clubs
+          {clubs === undefined ? '' : ` (${clubs.length})`}
+        </h2>
+        {renderClubList(clubs)}
+        <h2 className="sinc-section-header">
+          Members
+          {members === undefined ? '' : ` (${members.length})`}
+        </h2>
+        {renderMemberList(members)}
+      </div>
+    );
+
+    function renderClubList(clubs) {
+      if (!clubs) {
+        return <PageLoading />;
+      }
+      return (
+        <div>
+          {clubs.map(c => (
+            <div>{c.name}</div>
+          ))}
+        </div>
+      );
+    }
+
+    function renderMemberList(members) {
+      if (!members) {
+        return <PageLoading />;
+      }
+      return (
+        <MemberTable
+          rows={members}
+        />
+      );
+    }
+  }
+
+}
+
+ViewRegionDetail.contextTypes = {
+  router: React.PropTypes.object.isRequired,
+};
+
+function mapStateToProps(state) {
+  const clubs = state.regions.regionClubList;
+  const members = state.regions.regionMemberList;
+  const region = state.regions.regionDetail;
+  return {
+    clubs,
+    members,
+    region,
+  };
+}
+
+export default connect(mapStateToProps, {
+  fetchRegionClubList,
+  fetchRegionDetail,
+  fetchRegionMemberList,
+})(ViewRegionDetail);
