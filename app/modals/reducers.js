d7152776dce42d34b9ddef35ee186fa2b185629c
sdob <sdob@cs.ucc.ie>
2017-02-15 15:19:33 +0000

    Remove logging statements

--- app/modals/reducers.js
+++ app/modals/reducers.js
@@ -6,18 +6,12 @@ const initialState = {
 export default function modalReducer(state = initialState, action) {
   switch (action.type) {
     case 'SHOW_MODAL':
-      // console.info('caught by reducer');
-      // console.info('modalType');
-      // console.info(action.modalType);
-      // console.info('modalProps');
-      // console.info(action.modalProps);
       return {
         ...state,
         modalType: action.modalType,
         modalProps: action.modalProps,
       };
     case 'HIDE_MODAL':
-      console.info('caught HIDE_MODAL');
       return initialState;
     default:
       return state;
