ad23d36bda58a86751261bef74da1ec5b80fdee1
sdob <sdob@cs.ucc.ie>
2017-02-15 16:21:34 +0000

    Allow filtering clubs by region

--- app/clubs/ViewClubs.jsx
+++ app/clubs/ViewClubs.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as sort from 'sortabular';
 import { fetchRegions } from '../regions/actions';
-import { AddLink, PageLoading, SortedTable } from '../shared';
+import { AddLink, PageLoading, RegionFilter, SortedTable } from '../shared';
 import { roles } from '../profiles';
 import * as paths from '../paths';
 import { showModal } from '../modals/actions';
@@ -13,7 +13,12 @@ class ViewClubs extends Component {
 
   constructor(props, ctx) {
     super(props, ctx);
+    this.getVisibleClubs = this.getVisibleClubs.bind(this);
     this.handleDelete = this.handleDelete.bind(this);
+    this.onRegionToggle = this.onRegionToggle.bind(this);
+    this.state = {
+      regionVisibilities: {},
+    };
   }
 
   componentDidMount() {
@@ -21,6 +26,31 @@ class ViewClubs extends Component {
     // the 'Region' field in the table
     this.props.fetchRegions()
     .then(this.props.fetchClubList);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps !== this.props.regions) {
+      const visibilities = {};
+      nextProps.regions.forEach((region) => {
+        visibilities[region.id] = true;
+      });
+      this.setState({ regionVisibilities: visibilities });
+    }
+  }
+
+  getVisibleClubs() {
+    const { clubs } = this.props;
+    const { regionVisibilities } = this.state;
+    return clubs.filter(club => regionVisibilities[club.region.id]);
+  }
+
+  onRegionToggle(rid, value) {
+    this.setState({
+      regionVisibilities: {
+        ...this.state.regionVisibilities,
+        [rid]: value,
+      },
+    });
   }
 
   handleDelete(cid) {
@@ -36,7 +66,7 @@ class ViewClubs extends Component {
   }
 
   render() {
-    const { clubs, profile } = this.props;
+    const { clubs, profile, regions } = this.props;
 
     if (!(clubs && profile)) {
       return <PageLoading />;
@@ -51,8 +81,15 @@ class ViewClubs extends Component {
           {' '}
           {isAdmin && <AddLink to={paths.ADD_CLUB} />}
         </h1>
+        <h2 className="sinc-section-header sinc-section-header--minor">
+          Filter by region
+        </h2>
+        <RegionFilter regions={regions} onChange={this.onRegionToggle} />
+        <h2 className="sinc-section-header">
+          Results
+        </h2>
         <ClubTable
-          clubs={clubs}
+          clubs={this.getVisibleClubs()}
           editable={true}
           handleDelete={this.handleDelete}
           isAdmin={isAdmin}
