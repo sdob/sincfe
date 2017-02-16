ac0b111293efca61f7f4b46bd962b91fdcb81211
sdob <sdob@cs.ucc.ie>
2017-02-16 12:36:49 +0000

    Start defining route access by role

--- app/routes.jsx
+++ app/routes.jsx
@@ -16,6 +16,9 @@ import Login from './pages/Login';
 // Static landing page for authenticated users
 import Main from './pages/Main';
 
+// Higher-order components
+import { requireRole, roles } from './roles';
+
 // Other components; imports in alphabetical order, please
 import ActiveInstructors from './active-instructors/ActiveInstructors';
 import AddClub from './clubs/AddClub';
@@ -37,7 +40,6 @@ import MemberQualifications from './qualifications/MemberQualifications';
 import MedicalDisclaimer from './medical-disclaimer/MedicalDisclaimer';
 import MembershipHistory from './membership-history/MembershipHistory';
 import OrganizeCourses from './courses/OrganizeCourses';
-import RequireAuth from './auth/RequireAuth';
 import TeachCourses from './courses/TeachCourses';
 import ViewClubs from './clubs/ViewClubs';
 import ViewCourses from './courses/ViewCourses';
@@ -46,21 +48,37 @@ import ViewQualifications from './qualifications/ViewQualifications';
 import ViewRegions from './regions/ViewRegions';
 import ViewRegionDetail from './regions/ViewRegionDetail';
 
+// Define privileges
+const { Administrator, DiveOfficer, Member } = roles;
+const RequireAdmin = requireRole(Administrator);
+const RequireAuth = requireRole(Member);
+const RequireDiveOfficer = requireRole(DiveOfficer);
+const RequireAdminOrDiveOfficer = requireRole(Administrator.or(DiveOfficer));
+
 // Define and export app routes
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={RequireAuth(Main)} />
+    {/* Routes that require admin privileges */}
+    <Route path={paths.VIEW_CLUBS} component={RequireAdmin(ViewClubs)} />
+    <Route path={paths.VIEW_MEMBERS} component={RequireAdmin(ViewMembers)} />
+    <Route path={paths.ADD_CLUB} component={RequireAdmin(AddClub)} />
+
+    {/* Routes that require admin or DO privileges */}
+    <Route path={`${paths.EDIT_MEMBER}/:id`} component={RequireAdminOrDiveOfficer(EditMember)} />
+    <Route path={paths.ADD_MEMBER} component={RequireAdminOrDiveOfficer(AddMember)} />
+
+    {/* Routes that require DO privileges */}
+
+    {/* Routes that don't require admin privileges */}
     <Route path={paths.ACTIVE_INSTRUCTORS} component={RequireAuth(ActiveInstructors)} />
-    <Route path={paths.ADD_CLUB} component={RequireAuth(AddClub)} />
     <Route path={paths.ADD_COURSE} component={RequireAuth(AddCourse)} />
-    <Route path={paths.ADD_MEMBER} component={RequireAuth(AddMember)} />
     <Route path={paths.CLUB_QUALIFICATIONS} component={RequireAuth(ClubQualifications)} />
     <Route path={paths.CLUB_RENEWAL_ORDERS} component={RequireAuth(ClubRenewalOrders)} />
     <Route path={paths.CONTACT_US} component={RequireAuth(ContactUs)} />
     <Route path={paths.DOCUMENTS} component={RequireAuth(Documents)} />
     <Route path={`${paths.EDIT_CLUB}/:id`} component={RequireAuth(EditClub)} />
     <Route path={`${paths.EDIT_COURSE}/:id`} component={RequireAuth(EditCourse)} />
-    <Route path={`${paths.EDIT_MEMBER}/:id`} component={RequireAuth(EditMember)} />
     <Route path={`${paths.EDIT_QUALIFICATION}/:id`} component={RequireAuth(EditQualification)} />
     <Route path={`${paths.EDIT_REGION}/:id`} component={RequireAuth(ViewRegionDetail)} />
     <Route path={paths.EDIT_PROFILE} component={RequireAuth(EditProfile)} />
@@ -73,10 +91,8 @@ export default (
     <Route path={paths.ORGANIZE_COURSES} component={RequireAuth(OrganizeCourses)} />
     <Route path={paths.SEARCH_CURRENT_MEMBERS} component={RequireAuth(ClubMemberList)} />
     <Route path={paths.TEACH_COURSES} component={RequireAuth(TeachCourses)} />
-    <Route path={paths.VIEW_CLUBS} component={RequireAuth(ViewClubs)} />
     <Route path={paths.VIEW_COURSES} component={RequireAuth(ViewCourses)} />
     <Route path={`${paths.VIEW_COURSES}/:id`} component={RequireAuth(CourseDetail)} />
-    <Route path={paths.VIEW_MEMBERS} component={RequireAuth(ViewMembers)} />
     <Route path={paths.VIEW_REGIONS} component={RequireAuth(ViewRegions)} />
     <Route path={paths.VIEW_QUALIFICATIONS} component={RequireAuth(ViewQualifications)} />
     <Redirect path="*" to="/" />
