76ead7d649fdf91cf536747a7bc9c6098c13fa34
sdob <sdob@cs.ucc.ie>
2017-02-17 14:43:41 +0000

    Validate selects correctly (where '0' is valid)

--- app/courses/validate.js
+++ app/courses/validate.js
@@ -1,15 +1,15 @@
 import * as fields from './fields';
 
 export default function validate(values) {
-  const requiredFields = [
+  const requiredSelects = [
     fields.REGION,
     fields.CERTIFICATE,
   ];
 
   const errors = {};
-  requiredFields.forEach((field) => {
-    if (!values[field] || values[field] < 0) {
-      errors[field] = 'This field may not be blank.';
+  requiredSelects.forEach((field) => {
+    if (values[field] < 0) {
+      errors[field] = 'You must choose one.';
     }
   });
 
