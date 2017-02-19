14f2578f90863c52ec897eee9e8805cada6ec8ec
sdob <sdob@cs.ucc.ie>
2017-02-19 15:55:13 +0000

    Remove explicit use of default values

--- app/notifications/NotificationContainer.jsx
+++ app/notifications/NotificationContainer.jsx
@@ -19,62 +19,43 @@ export default function NotificationContainer() {
   return (
     <div className="sinc-notification-container">
       <FlashNotification
-        hideAfter={2000}
         message="Club created!"
-        status="success"
         trigger={clubAdd.success}
       />
       <FlashNotification
-        hideAfter={2000}
         message="Club deleted!"
-        status="success"
         trigger={clubDelete.success}
       />
       <FlashNotification
-        hideAfter={2000}
         message="Club updated!"
-        status="success"
         trigger={clubUpdate.success}
       />
       <FlashNotification
-        hideAfter={2000}
         message="Profile updated!"
-        status="success"
         trigger={profileTypes.PROFILE_UPDATE_SUCCESS}
       />
       <FlashNotification
-        hideAfter={2000}
         message="Member details updated!"
-        status="success"
         trigger={profileTypes.UPDATE_MEMBER_SUCCESS}
       />
       <FlashNotification
-        hideAfter={2000}
         message="Course added!"
-        status="success"
         trigger={courseAdd.success}
       />
       <FlashNotification
         message="Course updated!"
-        status="success"
         trigger={courseUpdate.success}
       />
       <FlashNotification
-        hideAfter={2000}
         message="Course deleted!"
-        status="success"
         trigger={courseDelete.success}
       />
       <FlashNotification
-        hideAfter={2000}
         message="Instructor added!"
-        status="success"
         trigger={courseInstructionAdd.success}
       />
       <FlashNotification
-        hideAfter={2000}
         message="Instructor removed!"
-        status="success"
         trigger={courseInstructionDelete.success}
       />
     </div>
