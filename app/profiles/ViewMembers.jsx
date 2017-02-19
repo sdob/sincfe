32509b4ad3faa59670fc42a1911e25d2cf6951e0
sdob <sdob@cs.ucc.ie>
2017-02-19 15:49:19 +0000

    Wait for user to start searching before loading members

--- app/profiles/ViewMembers.jsx
+++ app/profiles/ViewMembers.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import debounce from 'lodash/debounce';
+
 import * as paths from '../paths';
 import {
   AddLink,
@@ -8,27 +10,37 @@ import {
   RegionFilter,
 } from '../shared';
 import { fetchRegions } from '../regions/actions';
-import { fetchMembers } from './actions';
+import { fetchMembers, searchForMember } from './actions';
 
 class ViewMembers extends Component {
 
   constructor(props, ctx) {
     super(props, ctx);
     this.onRegionToggle = this.onRegionToggle.bind(this);
+    this.renderRegions = this.renderRegions.bind(this);
+    this.onSearchFieldChange = this.onSearchFieldChange.bind(this);
+    this.doDebouncedSearch = debounce(this.props.searchForMember, 250);
 
     this.state = {
+      inputIsEmpty: true,
       regionVisibilities: {},
     };
   }
 
   getVisibleMembers() {
     const { members } = this.props;
+    const { inputIsEmpty } = this.state;
+    // If the input is empty or we have no members to show, then
+    // return an empty list
+    if (inputIsEmpty || !members) {
+      return [];
+    }
+    // Otherwise, filter the users by the region filter
     const { regionVisibilities } = this.state;
     return members.filter(m => regionVisibilities[m.club.region.id]);
   }
 
   componentDidMount() {
-    this.props.fetchMembers();
     this.props.fetchRegions();
   }
 
@@ -53,22 +65,20 @@ class ViewMembers extends Component {
     });
   }
 
-  render() {
-    const { roles, members,regions } = this.props;
-
-    if (!members) {
-      return (
-        <div>
-          <h1 className="sinc-page-header d-flex justify-content-between">
-            View members
-            <AddLink to={paths.ADD_MEMBER} />
-          </h1>
-          <PageLoading />
-        </div>
-      );
+  onSearchFieldChange(e) {
+    if (e.target.value.length) {
+      this.setState({ inputIsEmpty: false });
+      this.doDebouncedSearch(e.target.value);
+    } else {
+      this.setState({ inputIsEmpty: true });
     }
+  }
+
+  render() {
+    const { roles, regions } = this.props;
 
     const visibleMembers = this.getVisibleMembers();
+
     // Because this is an admin view, there are members from many
     // clubs and regions. Add these as columns in the MemberTable.
     const extraColumns = {
@@ -79,16 +89,43 @@ class ViewMembers extends Component {
     return (
       <div>
         <h1 className="sinc-page-header d-flex justify-content-between">
-          View members ({visibleMembers.length} / {members.length})
+          View members
           <AddLink to={paths.ADD_MEMBER} />
         </h1>
-        <RegionFilter regions={regions} onChange={this.onRegionToggle} />
+        Start typing a user's name or CFT number to view matching results.
+        <div className="form-group row">
+          <div className="col-12">
+            <input
+              className="form-control"
+              onInput={this.onSearchFieldChange}
+              placeholder="Name or CFT number"
+            />
+          </div>
+        </div>
+        <h2 className="sinc-section-header sinc-section-header--minor">
+          Filter by region
+        </h2>
+        {this.renderRegions(regions)}
+        <h2 className="sinc-section-header">
+          Results ({visibleMembers.length})
+        </h2>
         <MemberTable
+          extraColumns={{
+            'club.name': { label: 'Club' },
+            'club.region.name': { label: 'Region' },
+          }}
           roles={roles}
           rows={visibleMembers}
         />
       </div>
     );
+  }
+
+  renderRegions(regions) {
+    if (!regions) {
+      return <PageLoading />;
+    }
+    return <RegionFilter regions={regions} onChange={this.onRegionToggle} />
   }
 }
 
@@ -101,7 +138,9 @@ function mapStateToProps(state) {
   };
 }
 
+
 export default connect(mapStateToProps, {
   fetchMembers,
   fetchRegions,
+  searchForMember,
 })(ViewMembers);
